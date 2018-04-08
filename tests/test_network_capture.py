f9fe474f55ed5fbef95559fef951f46c1dca4690
agnosticdev <agnosticdev@gmail.com>
2018-04-07 21:42:41 -0500

    master: Reworked project structure

---
+++ tests/test_network_capture.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+#
+# Network_Capture Module has been built as a standalone module 
+# This file is to test the functionality of Network_Capture.
+#
+
+import os, sys, unittest
