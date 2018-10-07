2e68546930fe039856bf0aac4881590424a7033a
agnosticdev <agnosticdev@gmail.com>
2018-10-06 19:12:18 -0500

    master: Improvements and the addition of asyncio

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Network Capture
 Python wrapper around tcpdump for fine grain search criteria that is written the final pcap.
 
-Network Capture was built as a Python wrapper around TCPDUMP to provide more advanced packet filtering in the output of your pcap or txt files.  Currently there is complexity on filtering the right information while using TCPDUMP, and this module attempts to normalize these problems by giving the user an open set of keywords and criteria to generate file based upon. 
+Network Capture was built as a Python wrapper around TCPdump to provide more advanced packet filtering in the output of your pcap or txt files.  Currently there is complexity on filtering the right information while using TCPdump, and this module attempts to normalize these problems by giving the user an open set of keywords and criteria to generate file based upon. 
 
 For example, when filtering on either the port, ip, or the interface you can specify the keywords that you are looking for in your traffic dump to be captured and written to the final output file. The final output will be a pcap and a txt file for review.
 
@@ -18,6 +18,6 @@ $ python network_capture.py -i en0 -keys error,host,ssl
 ```
 
 ## Note
-This module does not attempt to rebuild the functionality currently found in TCPDUMP, but rather to utilize it to make filtering stronger.
+This module does not attempt to rebuild the functionality currently found in TCPdump, but rather to utilize it to make filtering stronger.
 
-There are some built in validation routines available in this module that can validate common arguments like port number, host, and interface, but these are just extra utilities that attempt to add extra benefit on top of TCPDUMP. 
+There are some built in validation routines available in this module that can validate common arguments like port number, host, and interface, but these are just extra utilities that attempt to add extra benefit on top of TCPdump. 
--- network_capture.py
+++ network_capture.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 #
 # Network_Capture Module has been built as a standalone module 
-# Network Capture was built around TCPDump to provide more advanced packet
+# Network Capture was built around TCPdump to provide more advanced packet
 # filtering.  Currently there is complexity around the usage of filters and
-# arguments while using TCPDump.  This module aims to improve that. 
+# arguments while using TCPdump.  This module aims to improve that. 
 #
 #
 # Note: At the time of writing this script, it is intended for unix systems.
@@ -18,6 +18,7 @@
 #
 
 import os, sys, datetime, platform, ipaddress, fcntl, struct, subprocess
+import asyncio, pickle
 from socket import *
 
 # Host (199.99.99.99) capture for keys: error,host,ssl
@@ -27,98 +28,124 @@ from socket import *
 # $ python network_capture.py port 80 -keys error,host,ssl
 
 # Port (80) capture  for keys: error,host,ssl
-# $ python network_capture.py -i en0 -keys error,host,ssl
+# $ python network_capture.py -i enp4s0 ip6 -keys error,host,ssl
 
 #
 # TODO:
-#  1) Change all input commands over to tcpdump commands.
-#  2) Check for the -keys argument instead of -k 
-#  3) Save a full pcap, a filtered pcap, and a txt file.
-#
-#
+#  1) Generate valid TCPdump pcap (Binary stream is not valid right now)
+#  2) Add tests
+#  
 #
 
 
 class network_capture(object):
 
-	__slots__ = ('ip', 'port', 'interface', 
-				 'keywords', 'capture_cmd', 'filename')
+	__slots__ = ('keywords', 
+				 'capture_cmd', 
+				 'txt_file',
+				 'pcap_file',
+				 'validation_values',
+				 'validation_map')
 
 	def __init__(self, *args):
 
 		self.keywords = []
-		self.filename = self.get_filename()
 		self.capture_cmd = None
+		self.validation_values = {
+			'host': '',
+			'port': '',
+			'-i': ''
+		}
+		self.validation_map = {
+			'host': 'validate_host',
+			'port': 'validate_port',
+			'-i': 'validate_interface'
+		}
 
+		# Set both the filtered pcap and txt file names
+		filename = self.get_filename()
+		self.txt_file = filename + '.txt'
+		self.pcap_file = filename + '.pcap'
+
+
+		# Set args as an array of arrays
 		args = list(args)
+
+		# Make sure the list has usable arguments.
+		if len(args[0]) is 1:
+			exit("Please provide arguments to execute the program.")
 
 		# Make sure and validate the platform and the presence of tcpdump.
 		if not self.validate_platform():
 			exit("Make sure you are on a unix system with tcpdump installed.")
 
-		if "-ip" == args[0][1] and args[0][2] is not None:
-			self.ip = args[0][2]
-			if not self.validate_ip(self.ip):
-				exit("The ip being used is not valid.")
+		# Build the capture command passed in for TCPdump
+		self.capture_cmd = "sudo tcpdump"
 
-			self.capture_cmd = "sudo tcpdump host " + self.ip + " -vvv"
-		elif "-port" == args[0][1] and args[0][2] is not None:
-			self.port = args[0][2]
-			if not self.validate_port(self.port):
-				exit("The port being used is not valid.")
-
-			self.capture_cmd = "sudo tcpdump port " + self.port + " -vvv"
-		elif "-interface" == args[0][1] and args[0][2] is not None:
-			self.interface = args[0][2]
-			if not self.validate_interface(self.interface):
-				exit("The interface being used is not up.")
-
-			self.capture_cmd = "sudo tcpdump -i " + self.interface + " -vvv"
-
-		if "-k" == args[0][3] and args[0][4] is not None:
-			self.keywords = args[0][4].split(",")
-
-		# Perform the capture after all validation is complete
-		if self.capture_cmd is not None:
-			self.capture()
-		else:
-			exit("Something went wrong parsing the input.  Capture failed.")
+		# Iterate through the arguments to get the commands
+		for index, val in enumerate(args[0]):
+			if index > 0 and args[0][(index - 1)] == "-keys":
+				if "," not in val:
+					self.keywords.append(val)
+				else:
+					self.keywords = val.split(",")
+			elif index > 0 and val != '-keys':
+				self.capture_cmd += " {0}".format(val)
+				if val in self.validation_values:
+					self.validation_values[val] = args[0][(index + 1)]
 
 
+		# Set the verbose flags for TCPdump 
+		self.capture_cmd += " -vvv -XX"
+
+		# Validation check to make sure we have extracted -keys at this point
+		if len(self.keywords) == 0:
+			exit("Please make sure you provide keyword, exiting.")
+
+
+		# Iterate through the collected validation routines
+		# Make sure if the validation routines are available, they can be used.
+		for key, val in self.validation_values.items():
+			if val != '':
+				validate_method = getattr(self, self.validation_map[key])
+				validate_method(val)
+
+
+	# Platform validation
 	def validate_platform(self):
 		# This is only for unix system
 		if platform.system() is "Windows":
 			print("network_traffic is not supportd by Windows.")
 			return False
 
-		# TODO: validate tcpdump is on the machine
+		# TODO: validate TCPdump is on the machine
 
 		return True
 
-
-	def validate_ip(self, ip):
+	# Host validation
+	def validate_host(self, host):
 		try:
-			ip = ipaddress.ip_address(ip)
+			host = ipaddress.ip_address(host)
 		except ValueError:
-			message = f'IP: {ip!r} could not be set to IPv4 or IPv6.'
+			message = f'Host: {host!r} could not be set to IPv4 or IPv6.'
 			print(message)
-			return False
-		return True
+			exit("The host being used is not valid, exiting.")
 
+	# Port validation
 	def validate_port(self, port):
 		try:
 			port = int(port, 10)
 		except ValueError:
 			message = f'Port could not be cast to integer value as {port!r}'
 			print(message)
-			return False
+			exit("The port being used is not valid, exiting.")
 		if not ( 0 <= port <= 65535):
 			print("Port out of range 0-65535")
-			return False
-		
-		return True
-
+			exit("The port being used is not valid, exiting.")
+	
+	# Interface validation
 	def validate_interface(self, interface):
+		print("validate_interface!!!!")
 		SIOCGIFFLAGS = 0x8913
 		null256 = '\0'*256
 
@@ -127,46 +154,50 @@ class network_capture(object):
 		flags, = struct.unpack('H', fd[16:18])
 		unix_socket.close()
 		if flags & 1:
-			return True
+			pass
 		else:
-			return False
+			exit("The interface being used is not up.")
 
-
+	# Get a filename to set for both the pcap and txt filtered file
 	def get_filename(self):
-		return f"network_capture_{datetime.datetime.now():%Y-%m-%d-%m:%s}.txt"
+		return f"network_capture_{datetime.datetime.now():%Y-%m-%d-%m:%s}"
 
+	def capture_error(self):
+		print("capture_error")
 
-	def capture(self):
+	async def dispatch_capture(self):
 
 		print("Capturing command: {0}".format(self.capture_cmd))
+		print("Capturing keywords: {0}".format(self.keywords))
 		line_count = 0
-		captue_process = None
+		capture_pid = None
 		capture_stdout = None
 
 		# Open a file and start the capture with the files context for read/write
-		with open(self.filename, "w") as capture_file_object:
+		with open(self.txt_file, 'w') as txt_file_obj, \
+			open(self.pcap_file, 'wb') as pcap_file_obj:
 
 			try:
 				# Just to make sure you do not need to enter your password.
 				# Set the mode for the file to full read/write
-				os.system("chmod 777 " + self.filename)
-
-				# Start the capture
-				captue_process = subprocess.Popen([self.capture_cmd], 
-												  shell=True,
-												  stdout=subprocess.PIPE)
+				os.system("chmod 777 " + self.txt_file)
+				os.system("chmod 777 " + self.pcap_file)
 
 				print("-- Start Capturing Network Traffic --")
 
+				capture_pid = await self.capture_process()
 				while True:
-					captured_line = captue_process.stdout.readline()
+					captured_line = await self.capture_read_bytes(capture_pid)
+
 					if captured_line is not None and captured_line != b'':
+
 						captured_line = captured_line.decode("utf-8")
 						print("{0} \n".format(captured_line))
-						# Check for the keywords in the list 
 						if any(key in captured_line for key in self.keywords):
 							print("** Keyword found. Writing to log **")
-							capture_file_object.write(captured_line + "\n")
+							txt_file_obj.write(captured_line + "\n")
+							# Removing this until this works
+							#pickle.dump(captured_line, pcap_file_obj)
 							line_count += 1
 
 			except OSError as err:
@@ -174,22 +205,44 @@ class network_capture(object):
 				print("-- {0} lines captured in your filter --".format(line_count))
 				print("Error: {0}".format(err))
 				sys.exit(0)
-			except KeyboardInterrupt:
-				print("-- Exiting due to keyboard interrupt --")
+			except AttributeError as err:
+				print("-- Exiting due to an AttributeError --")
 				print("-- {0} lines captured in your filter --".format(line_count))
-				sys.exit(0)
+				print("Error: {0}".format(err))
 			except:
 				print("-- Unexpected excpetion received --")
 				print("-- {0} lines captured in your filter --".format(line_count))
 				print("Errors: {0}".format(sys.exc_info()[0]))
 				sys.exit(0)
 			finally:
-				capture_file_object.close()
-				captue_process.kill()
-				captue_process.wait()
+				txt_file_obj.close()
+				pcap_file_obj.close()
+				await self.kill_process(capture_pid)
+
+
+	async def capture_process(self):
+		return await asyncio.create_subprocess_shell(self.capture_cmd, 
+												  stdout=asyncio.subprocess.PIPE,
+												  stderr=asyncio.subprocess.PIPE)
+
+	async def capture_read_bytes(self, capture_pid):
+		return await capture_pid.stdout.readline()
+
+	async def kill_process(self, capture_pid):
+		capture_pid.kill()
+		await capture_pid.wait()
 
 
 
-nc = network_capture(sys.argv)
-nc.capture()
-
+if __name__ == '__main__':
+	# Create a new network capture object and pass in the system arguments
+	nc = network_capture(sys.argv)
+	# Create a new run loop
+	eventloop = asyncio.get_event_loop()
+	try:
+		# Execute capture until complete.
+		eventloop.run_until_complete(nc.dispatch_capture())
+	except KeyboardInterrupt as e:
+		print("-- Exiting due to keyboard interrupt --")
+	finally:
+		eventloop.close()
